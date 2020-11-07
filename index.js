2574890fd54445677b4e0f2b631180572aba95a6
LuanaGss <luana_gss@hotmail.com>
2020-11-07 11:47:14 -0300

    conclusão-do-projeto

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import cipher from './cipher.js';
 
 
-// botão criptografar ok
-   const toEncript = document.getElementById("toEncript")
-   toEncript.addEventListener("click", cipher.encode) 
+
+const toEncript = document.getElementById("toEncript")
+toEncript.addEventListener("click", (reset) => {
+   reset.preventDefault()
+   const text = document.getElementById("text").value.toUpperCase()
+   const offset = Number(document.getElementById("offset").value)
+
+   document.getElementById("reply").innerHTML = cipher.encode(offset, text);
+
+})
 
 
-// botão descriptografar ok   
-   const decrypt = document.getElementById("decrypt")
-   decrypt.addEventListener("click", cipher.decode) 
+
+const decrypt = document.getElementById("decrypt")
+decrypt.addEventListener("click", (reset) => {
+   reset.preventDefault()
+   const textTwo = document.getElementById("textTwo").value.toUpperCase()
+   const offsetTwo = Number(document.getElementById("offsetTwo").value)
+
+   document.getElementById("replyTwo").innerHTML = cipher.decode(offsetTwo, textTwo);
+
+})
+
 
 
 
