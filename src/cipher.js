42a4d179c1738543e2805a04839922ca54b6ab12
LuanaGss <luana_gss@hotmail.com>
2020-11-07 11:47:14 -0300

    conclusão-do-projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,52 +1,64 @@
 const cipher = {
-    encode:function () {
- 
-    
-    let text = document.getElementById("text").value.toUpperCase() //uma var. para guardar minha mensagem e deixar as letras maiúsculas
-    let offset = Number(document.getElementById("offset").value) //uma var. offset number para meu deslocamento.
-    let result = ""; //uma var vazia 
+  encode(offset, text) {
+
+    let result = "";
     const space = 32;
 
-  for(let i = 0; i < text.length; i++) { //um loop for para pegar toda a mensagem do usuário.
-    let codeAsc = text.charCodeAt(i) 
+    if (text == null || text == "" || offset == null || offset == "") {
+      throw new TypeError("É necessário inserir um texto", "cipher.js", 10);
 
-    if (codeAsc === space){
-      result += String.fromCharCode(space)
-    } else { const movement =((codeAsc - 65 + offset) % 26) + 65;
- result += String.fromCharCode(movement)
+    } else {
+      for (let i = 0; i < text.length; i++) {
+        let codeAsc = text.charCodeAt(i)
 
-    }
+        if (codeAsc === space) {
+          result += String.fromCharCode(space)
 
- }
-
-document.getElementById("reply").innerHTML = result;
-
-
-},
- decode: function () {
-
-    let text2 = document.getElementById("text2").value.toUpperCase()
-    let offset2 = Number(document.getElementById("offset2").value)
-    let resultTwo = "";
-    const spaceTwo = 32;
-
-  for(let i = 0; i < text2.length; i++) {
-        let codeAsc = text2.charCodeAt(i)
-
-        if (codeAsc === spaceTwo) {
-        resultTwo += String.fromCharCode(spaceTwo)
-
-        } else {const movementTwo =((codeAsc + 65 - offset2) % 26) + 65;
-        resultTwo += String.fromCharCode(movementTwo)
+        } else {
+          const movement = ((codeAsc - 65 + offset) % 26) + 65;
+          result += String.fromCharCode(movement)
 
         }
 
-        
+      }
+      return (result)
+
+
     }
 
-    document.getElementById("reply2").innerHTML = resultTwo;
 
- }
+  },
+  decode: function (offsetTwo, textTwo) {
+
+    let resultTwo = "";
+    const spaceTwo = 32;
+
+
+    if (textTwo == null || textTwo == "" || offsetTwo === null || offsetTwo === "") {
+      throw new TypeError("É necessário inserir um texto", "cipher.js", 10);
+
+    } else {
+      for (let i = 0; i < textTwo.length; i++) {
+        let codeAsc = textTwo.charCodeAt(i)
+
+        if (codeAsc === spaceTwo) {
+          resultTwo += String.fromCharCode(spaceTwo)
+
+        } else {
+          const movementTwo = ((codeAsc + 65 - offsetTwo) % 26) + 65;
+          resultTwo += String.fromCharCode(movementTwo)
+
+        }
+
+
+      }
+      return (resultTwo)
+
+
+    }
+
+
+  }
 
 
 }
@@ -55,4 +67,4 @@ export default cipher;
 
 
 
-  
+
